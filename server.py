0493d9479ec13dfe5266dd979d8e6d3d4fcf4b66
krustea <43367425+krustea@users.noreply.github.com>
2019-02-04 13:40:46 +0100

    Update server.py

--- server.py
+++ server.py
@@ -6,11 +6,10 @@ import time
 import threading
 import RPi.GPIO as GPIO
 
-
 app = Flask(__name__)
-degcel= TemperatureSensor()
+degcel = TemperatureSensor()
 socketio = SocketIO(app)
-broche=17
+broche = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 # setup des leds
@@ -30,29 +29,30 @@ def index():
 def message_loop():
     currentstate = 0
     previousstate = 0
-	Celsius = degcel.degreeCelsius()
-    while True:
-            # Lecture du capteur
-        currentstate = GPIO.input(broche)
-		 # Si le capteur est déclenché
-#        print("current state:"+ str(currentstate))
-        if currentstate == 1 and previousstate == 0:
-            GPIO.output(18, GPIO.HIGH)
-            GPIO.output(24, GPIO.HIGH)
-            GPIO.output(22, GPIO.HIGH)
-            message = Celsius
-            socketio.emit('alert', message, Broadcast=True)
-            previousstate = 1
-            print("mouvement")
+    Celsius = degcel.degreeCelsius()
+
+
+while True:
+    # Lecture du capteur
+    currentstate = GPIO.input(broche)
+    # Si le capteur est déclenché
+    #        print("current state:"+ str(currentstate))
+    if currentstate == 1 and previousstate == 0:
+        GPIO.output(18, GPIO.HIGH)
+        GPIO.output(24, GPIO.HIGH)
+        GPIO.output(22, GPIO.HIGH)
+        message = Celsius
+        socketio.emit('alert', message, Broadcast=True)
+        previousstate = 1
+        print("mouvement")
     # Si le capteur est s'est stabilisé
-        elif currentstate == 0 and previousstate == 1:
-            GPIO.output(18, GPIO.LOW)
-            GPIO.output(24, GPIO.LOW)
-            GPIO.output(22, GPIO.LOW)
-            previousstate = 0
+    elif currentstate == 0 and previousstate == 1:
+        GPIO.output(18, GPIO.LOW)
+        GPIO.output(24, GPIO.LOW)
+        GPIO.output(22, GPIO.LOW)
+        previousstate = 0
     # On attends 10ms
-        time.sleep(0.01)
-        
+    time.sleep(0.01)
 
 # Vue que notre méthode pour lire nos message est une boucle infinie
 # Elle bloquerait notre serveur. Qui ne pourrait répondre à aucune requête.
